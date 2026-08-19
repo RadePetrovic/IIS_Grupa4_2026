3b9cc6c39b4cf7f7ddaad859540e244d5d9e8fb0
Rade Petrovic <petrovicr380@gmail.com>
2026-08-19 13:58:37 +0200

    21 commit Implementirano brisanje linije, nastavak metode na tacu

--- src/gui/PnlDrawing.java
+++ src/gui/PnlDrawing.java
@@ -12,5 +12,14 @@ public void delete() {
 		} else {
 			JOptionPane.showMessageDialog(null, "Morate selektovati oblik!", "Upozorenje",
 					JOptionPane.WARNING_MESSAGE);
+			else if (selectedShape instanceof Line) {
+				int selectedOption = JOptionPane.showConfirmDialog(null, "Da li ste sigurni da zelite da obrisete liniju?",
+						"Upozorenje", JOptionPane.YES_NO_OPTION);
+				if (selectedOption == JOptionPane.YES_OPTION) {
+					shapes.remove(selectedShape);
+					selectedShape = null;
+					repaint();
+				}
+			}
 		}
 	}
