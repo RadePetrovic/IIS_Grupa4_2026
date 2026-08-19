092826d122466efcdd429eea2220ff8aecee1fbb
Rade Petrovic <petrovicr380@gmail.com>
2026-08-19 13:48:08 +0200

    commit 18 Povezana modifikacija sa dugmetom na glavnoj formi

--- src/gui/Drawing.java
+++ src/gui/Drawing.java
@@ -1,22 +1,17 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridBagLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import javax.swing.JToggleButton;
-import java.awt.Color;
-import java.awt.Dimension;
 import javax.swing.JToolBar;
-import javax.swing.SwingConstants;
-import java.awt.Font;
-import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Drawing extends JFrame {
 	
@@ -30,6 +25,10 @@ public class Drawing extends JFrame {
 	private JToggleButton tglbtnDonut = new JToggleButton("Donut");
 	private JToggleButton tglbtnSelect = new JToggleButton("Select");
 	
+	private JButton btnModify = new JButton("Modify");
+	private JButton btnDelete = new JButton("Delete");
+	
+	private final ButtonGroup btnGroup = new ButtonGroup();
 	private final JToolBar toolBar = new JToolBar();
 	private final JToolBar toolBar_1 = new JToolBar();
 	private final JLabel lbloblici = new JLabel("Oblici");
@@ -55,9 +54,41 @@ public class Drawing extends JFrame {
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		// Layout i dodavanje pnlDrawing, toolBar, toolBar_1, dugmadi...
+		contentPane.add(pnlDrawing, BorderLayout.CENTER);
+		
+		// Grupisanje dugmadi za crtanje i selekciju
+		btnGroup.add(tglbtnPoint);
+		btnGroup.add(tglbtnLine);
+		btnGroup.add(tglbtnRectangle);
+		btnGroup.add(tglbtnCircle);
+		btnGroup.add(tglbtnDonut);
+		btnGroup.add(tglbtnSelect);
+
+		toolBar.add(lbloblici);
+		toolBar.add(tglbtnPoint);
+		toolBar.add(tglbtnLine);
+		toolBar.add(tglbtnRectangle);
+		toolBar.add(tglbtnCircle);
+		toolBar.add(tglbtnDonut);
+		
+		contentPane.add(toolBar, BorderLayout.NORTH);
+
+		toolBar_1.add(lblNewActions);
+		toolBar_1.add(tglbtnSelect);
+		toolBar_1.add(btnModify);
+		toolBar_1.add(btnDelete);
+		
+		contentPane.add(toolBar_1, BorderLayout.SOUTH);
+
+		// Povezivanje dugmeta Modify sa pnlDrawing.modify()
+		btnModify.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pnlDrawing.modify();
+			}
+		});
 	}
 
 	public PnlDrawing getPnlDrawing() {
@@ -68,11 +99,39 @@ public class Drawing extends JFrame {
 		this.pnlDrawing = pnlDrawing;
 	}
 
+	public JToggleButton getTglbtnPoint() {
+		return tglbtnPoint;
+	}
+
+	public JToggleButton getTglbtnLine() {
+		return tglbtnLine;
+	}
+
+	public JToggleButton getTglbtnRectangle() {
+		return tglbtnRectangle;
+	}
+
+	public JToggleButton getTglbtnCircle() {
+		return tglbtnCircle;
+	}
+
+	public JToggleButton getTglbtnDonut() {
+		return tglbtnDonut;
+	}
+
 	public JToggleButton getTglbtnSelect() {
 		return tglbtnSelect;
 	}
 
 	public void setTglbtnSelect(JToggleButton tglbtnSelect) {
 		this.tglbtnSelect = tglbtnSelect;
+	}
+
+	public JButton getBtnModify() {
+		return btnModify;
+	}
+
+	public JButton getBtnDelete() {
+		return btnDelete;
 	}
 }
